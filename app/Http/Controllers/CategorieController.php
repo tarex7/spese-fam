4a909dfa417dd80fd13f51d3f811cda6400c66da
tarex7 <tarex7@gmail.com>
2023-10-11 11:23:25 +0200

    phpstorm

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -8,14 +8,7 @@ use Illuminate\Http\Request;
 
 class CategorieController extends Controller
 {
-    // public function index() {
-    //    $categorie = new Categorie();
-
-    //    $x = $categorie->list();
-
-    //    return view('categorie.categorie');
-
-    // }
+    
 
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,12 @@ Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index')->middleware('auth');
 
+
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/', 'SpeseController@index')->name('spese');
 
-    // Route::any('/spese/salva', 'SpeseController@salva')->name('spese');
+    //SPESE
+    Route::get('/', 'SpeseController@index')->name('spese');
     Route::any('/spese/salva', 'SpeseController@salva')->name('spese/salva');
     Route::any('/spese/aggiungi', 'SpeseController@aggiungi')->name('spese/aggiungi');
     Route::any('/spese/elimina/{id}', 'SpeseController@elimina')->name('spese/elimina');
@@ -32,15 +33,17 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/categorie', 'CategorieController@index')->name('categorie');
     Route::any('/categorie/elimina/{id}', 'categorieController@elimina')->name('categorie/elimina');
     Route::any('/categorie/salva', 'CategorieController@salva')->name('categorie/salva');
-
     Route::any('/categorie/aggiungi', 'CategorieController@aggiungi')->name('categorie/aggiungi');
+
+    //CATEGORIE ENTRATE
+    Route::get('/categorie_entrate', 'CategorieEntrateController@index')->name('categorie_entrate');
+    Route::any('/categorie_entrate/elimina/{id}', 'CategorieEntrateController@elimina')->name('categorie_entrate/elimina');
+    Route::any('/categorie_entrate/salva', 'CategorieEntrateController@salva')->name('categorie_entrate/salva');
+    Route::any('/categorie_entrate/aggiungi', 'CategorieEntrateController@aggiungi')->name('categorie_entrate/aggiungi');
+
 
     //TIPOLOGIA
     Route::get('/tipologia', 'TipologiaController@index')->name('tipologia');
-    //Route::get('/spese/nuovo', 'SpeseController@nuovo')->name('spese/nuovo');
-
-
-    // Route::get('/home', 'HomeController@index')->name('home');
 });
 
 
